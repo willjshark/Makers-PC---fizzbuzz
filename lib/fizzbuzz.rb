05c973a1d3299ba0ae49629c917459839ebab14d
dean <d.lewis.rave@gmail.com>
2021-10-14 11:55:55 +0100

    fizzbuzz program

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,14 @@
 def fizzbuzz(number)
-  'fizz'
+  (1..number).each do |x|
+    if x % 3 == 0 && x % 5 == 0
+      puts "fizzbuzz"
+    elsif x % 3 == 0
+      puts "fizz"
+    elsif x % 5 == 0
+      puts "buzz"
+    else
+      puts x
+    end
+  end
 end
-
+fizzbuzz(20)

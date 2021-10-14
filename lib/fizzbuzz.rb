6de60e9df26970109ec38965f62a0675ce4ca199
dean <d.lewis.rave@gmail.com>
2021-10-14 14:29:51 +0100

    7 green test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
-    puts "fizzbuzz"
+    return "fizzbuzz"
   elsif number % 3 == 0
-    puts "fizz"
+    return "fizz"
   elsif number % 5 == 0
-    puts "buzz"
+    return "buzz"
   else
     return number
   end
 end
-fizzbuzz(10)
+fizzbuzz(7)
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -14,4 +14,23 @@ describe "fizzbuzz" do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 end
-
+describe "fizzbuzz" do
+  it 'returns "fizzbuzz" when passed a number is divisable by 3 and 5' do
+    expect(fizzbuzz(90)).to eq 'fizzbuzz'
+  end
+end 
+describe "fizzbuzz" do
+  it 'returns 7 since it is indivisible by 3 and 5' do
+    expect(fizzbuzz(7)).to eq 7
+  end
+end
+describe "fizzbuzz" do
+  it 'returns "fizz" when passed a number divisable by 3' do
+    expect(fizzbuzz(99)).to eq 'fizz'
+  end
+end
+describe "fizzbuzz" do
+  it 'returns "buzz" when pass a number divisable by 5' do
+    expect(fizzbuzz(80)).to eq 'buzz'
+  end
+end
